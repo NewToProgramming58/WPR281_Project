3813d4df0483ef726c87117f622d34685109bb42
NewToProgramming58 <NewToProgramming58@users.noreply.github.com>
2022-08-31 17:48:30 +0200

    Update projectManipulation.js

--- Projects/projectManipulation.js
+++ Projects/projectManipulation.js
@@ -1,5 +1,19 @@
 window.onload = function() {
     LoadProjects();
+    let arrUsers = JSON.parse(window.localStorage.getItem("users"));
+    let loggedID = window.localStorage.getItem("loggedInUser");
+    for (let i = 0; i < arrUsers.length; i++) {
+        if (arrUsers[i]["id"] == loggedID) {
+            currentUser = {
+                "id": arrUsers[i]["id"],
+                "name": arrUsers[i]["name"],
+                "surname": arrUsers[i]["surname"],
+                "profilePicture": arrUsers[i]["profilePicture"],
+            }
+        }
+    }
+    document.getElementById('welcome').innerHTML = `Welcome ${currentUser.name} to the bug tracking site!`;
+    document.getElementById('profileImage').src = currentUser.profilePicture;
 }
 
 function LoadProjects(){
