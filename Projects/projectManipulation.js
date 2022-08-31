2fc1d59135b2edd37fdf9a03c27b8ed85ffb0c79
NewToProgramming58 <NewToProgramming58@users.noreply.github.com>
2022-08-31 19:30:02 +0200

    MVP 1

--- Projects/projectManipulation.js
+++ Projects/projectManipulation.js
@@ -45,17 +45,48 @@ function LoadProjects(){
                     tabCell.innerHTML = arrProjects[i][key];        
                 }              
             }
+            if (table) {
+                for (var i = 0; i < table.rows.length; i++) {
+                  table.rows[i].onclick = function() {
+                        projectTableID = this.childNodes[0].innerHTML;
+                        document.getElementById('Name').value = this.childNodes[1].innerHTML;;
+                        document.getElementById('ShortDescription').value =this.childNodes[2].innerHTML;
+                        HighlightRow(table)
+                  };
+                }
+            }
+                
         }      
+    }
+}
+
+function HighlightRow(table) {
+    var cells = table.getElementsByTagName('td');
+    for(var i = 0; i < cells.length; i++){
+        // Get individual cell
+        var cellVals = cells[i];
+        // onclick event highlights
+        cellVals.onclick = function () {
+            // Get row id for cell
+            var rowsNotSelected = table.getElementsByTagName('tr');
+            var rowId = this.parentNode.rowIndex;
+            for(let row = 0; row < rowsNotSelected.length; row++){
+                rowsNotSelected[row].style.backgroundColor = "";
+                rowsNotSelected[row].classList.remove('selected');
+            }
+            var selectedRow = table.getElementsByTagName('tr')[rowId];
+            selectedRow.style.backgroundColor = "aliceblue";
+            selectedRow.className += " selected"; // Don't understand this; will keep searching
+        }
     }
 }
 
 function AddProject(edit){
     //Get values for new project
-    var issueName = document.getElementById('IssueName').value;
     var projectName = document.getElementById('Name').value;
-    var description = document.getElementById('description').value;
+    var description = document.getElementById('ShortDescription').value;
     
-    if (projectName === '' || issueName === '' || description === '') {
+    if (projectName === '' || description === '') {
         alert('Please enter all fields');
     } else {
         //get project from storage
@@ -104,7 +135,7 @@ function RemoveProject(){
             if (length > 1) {
             let i = -1;
                 let projectExists = false;
-                while (i < length && !bugExists) {
+                while (i < length && !projectExists) {
                     i++;
                     if (projects[i]['id'] == projectTableID) {
                         projectExists = true;            
